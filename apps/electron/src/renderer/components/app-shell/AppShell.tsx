8658d5b0cbe88227356192ddbbb668addd9a674b
1cao2mei3nai4lao <152588759+1cao2mei3nai4lao@users.noreply.github.com>
2026-09-16 11:56:17 +0800

    fix: remove creation jobs buttons from navigation

--- apps/electron/src/renderer/components/app-shell/AppShell.tsx
+++ apps/electron/src/renderer/components/app-shell/AppShell.tsx
@@ -32,7 +32,7 @@ import { SquarePenRounded } from "../icons/SquarePenRounded"
 import { McpIcon } from "../icons/McpIcon"
 import { Button } from "@/components/ui/button"
 import { HeaderIconButton } from "@/components/ui/HeaderIconButton"
-import { CreationJobsButton, CreationJobsHost } from "./CreationJobsButton"
+import { CreationJobsHost } from "./CreationJobsButton"
 import type { CreationJob } from "@/atoms/creation-jobs"
 import { clearProjectFilter, resolveNewSessionParams, resolveProjectNavigationSessionId, type FilterMode } from "./inherited-filter-params"
 import { filterSessionsByProject, getIncludedProjectName, hasIncludedProjectFilter } from "./project-session-filter"
@@ -2159,11 +2159,6 @@ function AppShellContent({
                         tooltip={t("fileImport.mcpTitle")}
                         onClick={() => setMcpFileImportOpen(true)}
                       />
-                      <CreationJobsButton
-                        workspaceId={activeWorkspace.id}
-                        onReopen={reopenCreationJob}
-                        onOpenResult={openCreationResult}
-                      />
                       <EditPopover
                         trigger={
                           <HeaderIconButton
@@ -2209,11 +2204,6 @@ function AppShellContent({
                   {/* Add Automation button (only for automations mode) */}
                   {isAutomationsNavigation(navState) && activeWorkspace && (
                     <>
-                      <CreationJobsButton
-                        workspaceId={activeWorkspace.id}
-                        onReopen={reopenCreationJob}
-                        onOpenResult={openCreationResult}
-                      />
                       <EditPopover
                         key={`${activeWorkspace.id}:${automationCreationKey(automationFilter?.automationType)}`}
                         trigger={
